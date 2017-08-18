e830894d66b18d7978112f0a82be27d74a80609b
dracominer <dracominer947@gmail.com>
2017-08-18 14:07:28 -0500

    Better Not Decay
    and cleaner console (removed printing update to console.)
    Still some weird behaviour in the code though. removing the call to
    System.out made it only play one note. But calling any static method in
    System.out makes it work again.. I don't get it.

--- Dynamic BG Music Generator/src/dna/audio/synthesis/MusicScale.java
+++ Dynamic BG Music Generator/src/dna/audio/synthesis/MusicScale.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 public class MusicScale {
 
-	public static MusicScale Pentatonic = new MusicScale("Penatonic", 4, 'c', 'd', 'e', 'f', 'g', 'c');
+	public static final MusicScale Pentatonic = new MusicScale("Penatonic", 4, 'c', 'd', 'e', 'f', 'g', 'c');
+	public static final MusicScale PhrygianDominant = new MusicScale("PhrygianDominant", 4, 'c', 'd', '-', 'e', 'f', 'g', 'a', '-', 'b', '-', 'c');
+	/**The pythagorean tuning of the chromatic scale*/
+	public static final MusicScale Chromatic = new MusicScale("PhrygianDominant", 4, 'c', 'd', '-', 'c', '+', 'd', 'e', '-', 'd', '+', 'e', 'f', 'f', '+', 'g', '-', 'g', 'a', '-', 'g', '+', 'a', 'b', '-', 'a', '+', 'b', 'c');
 
 	private NoteData[] acceptedNotes;
 	private String name;
@@ -17,15 +20,15 @@ public class MusicScale {
 
 	public MusicScale(String name, int startOctave, char... notes) {
 		this.name = name;
-		List<NoteData> data = new ArrayList<NoteData>();
+		List<NoteData> data = new ArrayList<NoteData>(notes.length);
 		int octave = startOctave;
 		int dataIndex = 0;
 		for (int i = 0; i < notes.length; i++) {
 			char note = Character.toLowerCase(notes[i]);
 			if (i != 0 && note == 'c') octave++;
 			if (Character.isLetter(note)) {
+				if (i != 0) dataIndex++;
 				data.add(new NoteData(octave, note));
-				dataIndex++;
 			} else {
 				if (note == '+') data.set(dataIndex, new NoteData(octave, notes[i - 1], NoteHelper.Sharp));
 				if (note == '-') data.set(dataIndex, new NoteData(octave, notes[i - 1], NoteHelper.Flat));
--- Dynamic BG Music Generator/src/dna/audio/synthesis/SynthWave.java
+++ Dynamic BG Music Generator/src/dna/audio/synthesis/SynthWave.java
@@ -1,6 +1,8 @@
 package dna.audio.synthesis;
 
 public abstract class SynthWave extends SynthGen {
+
+	public static final float DecayVolOffset = 0.001f;
 	protected double volume = 5000;
 	protected double decay = 5;
 	protected double frequency = 970.0;
@@ -30,7 +32,15 @@ public abstract class SynthWave extends SynthGen {
 	}
 
 	public double getDecayedVolume(double time) {
-		return volume * (Math.pow(Math.E, -time * decay));
+		return volume * (Math.pow(2, -time * decay) - DecayVolOffset);
+	}
+
+	public static float getDecayLevelForNoteLength(float length) {
+		double a = Math.log10(DecayVolOffset);
+		double b = (Math.log10(2) * -length);
+		if (b == 0) b = 0.00001;
+		double r = a / b;
+		return (float) r;
 	}
 
 }
